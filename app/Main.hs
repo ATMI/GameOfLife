d2995090fb431b7bb3f0e732e86eb9605d9ce26f
a <artem.myasoedov@gmail.com>
2023-06-27 20:26:11 +0300

    Added gloss "Hello, World!" window

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,10 @@
 module Main (main) where
 
-import Lib
+import Graphics.Gloss
 
 main :: IO ()
-main = someFunc
+main = display window background picture
+  where
+    window = InWindow "Hello, Gloss!" (800, 400) (100, 100)
+    background = white
+    picture = scale 0.5 0.5 $ color black $ text "Hello, World!"
